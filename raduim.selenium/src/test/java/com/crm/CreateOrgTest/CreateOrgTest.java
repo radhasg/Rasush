14dd1a0674e67f1295dec08e0d4dc34965d79d4c
radha <radha.sg2014@gmail.com>
2022-03-28 16:55:46 +0530

    fgdh

---
+++ raduim.selenium/src/test/java/com/crm/CreateOrgTest/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package com.crm.CreateOrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest{
+	@Test
+	public void createOrg() {
+		System.out.println("created org");
+		
+		System.out.println("created has done");
+	}
+
+}
---
+++ raduim.selenium/src/test/java/com/crm/CreateOrgTest/CreateOrgWithIndustry.java
@@ -0,0 +1,11 @@
+package com.crm.CreateOrgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithIndustry {
+	@Test
+	public void createOrgWithIndustry() {
+		System.out.println("created org with industry");
+	}
+
+}
